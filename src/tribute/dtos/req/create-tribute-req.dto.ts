39ebb56bed3e39b51b4e72893caf18f96f1e58a2
parksangwoo <b01056721617@gmail.com>
2024-05-19 17:06:29 +0900

    chore: date 형변환 로직 추가

--- src/tribute/dtos/req/create-tribute-req.dto.ts
+++ src/tribute/dtos/req/create-tribute-req.dto.ts
@@ -1,3 +1,4 @@
+import { Type } from 'class-transformer';
 import {
   IsBoolean,
   IsDate,
@@ -29,5 +30,6 @@ export class CreateTributeReqDto {
 
   @IsNotEmpty()
   @IsDate()
+  @Type(() => Date)
   date: Date;
 }
